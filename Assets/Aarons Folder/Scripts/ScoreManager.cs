017cd9f42fd81d9c2a6372ab833f0924ee875fd6
Aaron K Arseneau <aaronarseneau@my.unt.edu>
2025-03-29 20:17:49 -0500

    Server

--- Assets/Aarons Folder/Scripts/ScoreManager.cs
+++ Assets/Aarons Folder/Scripts/ScoreManager.cs
@@ -5,13 +5,11 @@ using UnityEngine.Events;
 public class ScoreManager : MonoBehaviour
 {
     public int inputScore = 0;
-    [SerializeField]
-    private TMP_InputField inputName;
 
-    public UnityEvent<string, int> submitScoreEvent;
+    public UnityEvent<int> submitScoreEvent;
 
     public void SubmitScore()
     {
-        submitScoreEvent.Invoke(inputName.text, inputScore);
+        submitScoreEvent.Invoke(inputScore);
     }
 }
